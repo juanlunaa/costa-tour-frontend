3afac2678f8712da040ae62a8408c9d46d4ee771
juanlunaa <jp3165239@gmail.com>
2024-09-06 21:53:50 -0500

    feat: add layout to auth pages

--- src/app/auth/layout.jsx
+++ src/app/auth/layout.jsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import { IoMdArrowBack } from "react-icons/io";
+
 export default function AuthLayout ({ children }) {
   return(
-    <main className="min-h-screen bg-gray-500">
-      {children}
+    <main className="h-screen sm:flex sm:flex-row sm:min-h-screen">
+      <div className='relative h-40 w-full sm:w-1/2 sm:min-h-screen'>
+        <Image
+          src='/auth-image.webp'
+          objectFit='cover'
+          layout='fill'
+          alt='Cartagena'
+        />
+
+        <Link href='/' className='absolute top-2 left-2'>
+          <IoMdArrowBack className='text-3xl sm:text-5xl' />
+        </Link>
+      </div>
+      <div className='flex flex-col items-center justify-center sm:w-1/2 pt-12 sm:pt-0'>
+        {children}
+      </div>
     </main>
   )
 }
