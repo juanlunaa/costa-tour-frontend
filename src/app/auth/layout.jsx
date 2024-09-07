b185d863d60449b11e9c6fbd66045757a63bc8ac
juanlunaa <jp3165239@gmail.com>
2024-09-07 17:53:45 -0500

    add change image orientation of login page to register page

--- src/app/auth/layout.jsx
+++ src/app/auth/layout.jsx
@@ -1,10 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { IoMdArrowBack } from "react-icons/io";
 
 export default function AuthLayout ({ children }) {
+
+  const pathname = usePathname();
+
+  const orientacion = pathname === '/auth/login' ? 'sm:flex-row' : 'sm:flex-row-reverse';
+
   return(
-    <main className="h-screen sm:flex sm:flex-row sm:min-h-screen">
+    <main className={`sm:flex ${orientacion} sm:min-h-screen`}>
       <div className='relative h-40 w-full sm:w-1/2 sm:min-h-screen'>
         <Image
           src='/auth-image.webp'
