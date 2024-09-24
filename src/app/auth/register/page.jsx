c4d1e22f56f2491b442123e2648a8471c3d41060
juanlunaa <jp3165239@gmail.com>
2024-09-24 16:56:36 -0500

    connet register view with backend endpoint, using react hook

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,51 +1,91 @@
-import Link from "next/link"
+"use client"
+
 import { titleFont, textFont } from "@/config/fonts"
+import useLocationData from "@/hooks/useLocationData"
+import { fecthEstadosByPais, fetchCiudadesByEstado, fetchPaises } from "@/services/location"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
+import { AiOutlineIdcard } from "react-icons/ai"
+import { CiCalendarDate } from "react-icons/ci"
 import { FaRegUser } from "react-icons/fa"
 import { MdOutlineEmail } from "react-icons/md"
 import { RiLockPasswordLine } from "react-icons/ri"
-import { AiOutlineIdcard } from "react-icons/ai"
-import { IoPhonePortraitOutline } from "react-icons/io5"
-import { CiCalendarDate } from "react-icons/ci"
 import { TiWorld } from "react-icons/ti"
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const { locationData, locationSelect, handleChange } = useLocationData()
+
+  const server = process.env.NEXT_PUBLIC_BACKEND_SERVER
+
+  const onSubmit = handleSubmit(async (data) => {
+    const newData = { ...data, intereses: [1, 2, 3] }
+    const res = await fetch(`${server}/turist/create`, {
+      method: "POST",
+      body: JSON.stringify(newData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    const resJSON = await res.json()
+    console.log(resJSON)
+  })
+
   return (
     <>
       <h1 className={`${titleFont.className} font-bold text-2xl text-center`}>CREAR CUENTA</h1>
 
-      <form action="" className="flex flex-col items-center gap-4 w-[90%] mt-4">
+      <form onSubmit={onSubmit} className="flex flex-col items-center gap-4 w-[90%] mt-4">
         <div className="grid grid-cols-2 gap-2">
           <div className="relative">
             <input
               placeholder="Nombre"
               className="bg-[#D1EEF2] p-3 rounded-lg pl-10 w-full placeholder:text-black/60"
+              {...register("nombre", { required: true })}
             />
 
             <FaRegUser className="absolute top-1/2 left-3 transform -translate-y-1/2 text-2xl" />
           </div>
 
-          <input placeholder="Apellido" className="bg-[#D1EEF2] p-3 rounded-lg w-full placeholder:text-black/60" />
+          <input
+            placeholder="Apellido"
+            className="bg-[#D1EEF2] p-3 rounded-lg w-full placeholder:text-black/60"
+            {...register("apellido", { required: true })}
+          />
 
           <div className="relative">
             <input
-              type="email"
-              placeholder="Tipo Documento"
+              placeholder="N° Identificación"
               className="bg-[#D1EEF2] p-3 rounded-lg pl-10 w-full placeholder:text-black/60"
+              {...register("dni", { required: true })}
             />
 
             <AiOutlineIdcard className="absolute top-1/2 left-3 transform -translate-y-1/2 text-2xl" />
           </div>
 
-          <input
-            placeholder="N° Identificación"
-            className="bg-[#D1EEF2] p-3 rounded-lg w-full placeholder:text-black/60"
-          />
+          <div className="relative">
+            <input
+              type="date"
+              placeholder="Fecha Nacimiento"
+              className="bg-[#D1EEF2] p-3 rounded-lg pl-10 w-full placeholder:text-black/60"
+              {...register("fechaNacimiento", { required: true })}
+            />
+
+            <CiCalendarDate className="absolute top-1/2 left-3 transform -translate-y-1/2 text-2xl" />
+          </div>
 
           <div className="relative col-span-2">
             <input
               type="email"
               placeholder="Email"
               className="bg-[#D1EEF2] p-3 rounded-lg pl-10 w-full placeholder:text-black/60"
+              {...register("email", { required: true })}
             />
 
             <MdOutlineEmail className="absolute top-1/2 left-3 transform -translate-y-1/2 text-2xl" />
@@ -56,47 +96,57 @@ export default function Register() {
               type="password"
               placeholder="Contraseña"
               className="bg-[#D1EEF2] p-3 rounded-lg pl-10 w-full placeholder:text-black/60"
+              {...register("password", { required: true })}
             />
 
             <RiLockPasswordLine className="absolute top-1/2 left-3 transform -translate-y-1/2 text-2xl" />
           </div>
 
-          <div className="relative">
-            <input
-              type="email"
-              placeholder="Telefono"
-              className="bg-[#D1EEF2] p-3 rounded-lg pl-10 w-full placeholder:text-black/60"
-            />
-
-            <IoPhonePortraitOutline className="absolute top-1/2 left-3 transform -translate-y-1/2 text-2xl" />
-          </div>
-
-          <div className="relative">
-            <input
-              type="email"
-              placeholder="Fecha Nacimiento"
-              className="bg-[#D1EEF2] p-3 rounded-lg pl-10 w-full placeholder:text-black/60"
-            />
-
-            <CiCalendarDate className="absolute top-1/2 left-3 transform -translate-y-1/2 text-2xl" />
-          </div>
-
           <div className="relative col-span-2">
-            <input
-              type="email"
-              placeholder="Pais"
+            <select
+              name="pais"
               className="bg-[#D1EEF2] p-3 rounded-lg pl-10 w-full placeholder:text-black/60"
-            />
+              value={locationSelect.pais}
+              onChange={handleChange}
+            >
+              <option value="0">Selecciona un pais</option>
+              {locationData.paises.map((p) => (
+                <option value={p.id} key={p.id}>
+                  {p.name}
+                </option>
+              ))}
+            </select>
 
             <TiWorld className="absolute top-1/2 left-3 transform -translate-y-1/2 text-2xl" />
           </div>
 
-          <input
-            placeholder="Estado/Departamento"
+          <select
+            name="estado"
             className="bg-[#D1EEF2] p-3 rounded-lg w-full placeholder:text-black/60"
-          />
+            value={locationSelect.estado}
+            onChange={handleChange}
+          >
+            <option value="0">Selecciona un estado</option>
+            {locationData.estados.map((e) => (
+              <option value={e.id} key={e.id}>
+                {e.name}
+              </option>
+            ))}
+          </select>
 
-          <input placeholder="Ciudad" className="bg-[#D1EEF2] p-3 rounded-lg w-full placeholder:text-black/60" />
+          <select
+            className="bg-[#D1EEF2] p-3 rounded-lg w-full placeholder:text-black/60"
+            {...register("idCiudad", { required: true })}
+          >
+            <option value="0" default>
+              Selecciona una ciudad
+            </option>
+            {locationData.ciudades.map((c) => (
+              <option value={c.id} key={c.id}>
+                {c.name}
+              </option>
+            ))}
+          </select>
         </div>
 
         <button
