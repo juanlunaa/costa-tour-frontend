775fd0d914c9226ebbc4d12b6fd1f354df39ad07
juanlunaa <jp3165239@gmail.com>
2024-09-28 00:28:17 -0500

    create custom hook to better provide global status data (user)

---
+++ src/hooks/useUserData.js
@@ -0,0 +1,14 @@
+import { useUserStore } from "@/store/user"
+import { useEffect } from "react"
+import Cookies from "js-cookie"
+
+export function useUserData() {
+  const user = useUserStore(state => state.user)
+  const isLoggedIn = useUserStore(state => state.isLoggedIn)
+  const fetchUserProfile = useUserStore(state => state.fetchUserProfile)
+  const signInUser = useUserStore(state => state.signInUser)
+  const signUpUser = useUserStore(state => state.signUpUser)
+  const logoutUser = useUserStore(state => state.logoutUser)
+
+  return { user, isLoggedIn, fetchUserProfile, signInUser, signUpUser, logoutUser}
+}
