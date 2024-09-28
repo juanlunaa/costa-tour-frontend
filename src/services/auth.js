d5b67a2ab204cbc7c6324ba4568ccdab33666f39
juanlunaa <jp3165239@gmail.com>
2024-09-28 00:21:49 -0500

    extract request logic for the login procces (sign in, sign up, get profile, logout)

---
+++ src/services/auth.js
@@ -0,0 +1,57 @@
+const server = process.env.NEXT_PUBLIC_BACKEND_SERVER
+
+export const login = async (data) => {
+  const res = await fetch(`${server}/user/auth`, {
+    method: "POST",
+    credentials: 'include',
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+
+  const resJson = await res.json()
+
+  return { res: resJson, status: res.status }
+}
+
+export const registerTurist = async (data) => {
+  const res = await fetch(`${server}/turist/create`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  
+  const resJson = await res.json()
+
+  return { res: resJson, status: res.status }
+}
+
+export const fetchProfile = async () => {
+  const res = await fetch(`${server}/user/profile`, {
+    method: "GET",
+    credentials: 'include',
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+
+  const resJson = await res.json()
+  return { res: resJson, status: res.status }
+}
+
+export const logout = async () => {
+  const res = await fetch(`${server}/user/logout`, {
+    method: "POST",
+    credentials: 'include',
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+
+  const resJson = await res.json()
+
+  return { res: resJson, status: res.status }
+}
