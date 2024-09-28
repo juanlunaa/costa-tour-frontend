d4c4192013df97e92a93539db325914d07dc5280
juanlunaa <jp3165239@gmail.com>
2024-09-28 15:06:55 -0500

    create a react context to request the user data on the first render

---
+++ src/context/user.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { useUserStore } from '@/store/user';
+import Cookies from 'js-cookie';
+import { createContext, useEffect } from 'react'
+
+export const UserContext = createContext()
+
+export function UserProvider ({ children }) {
+  const fetchUserProfile = useUserStore((state) => state.fetchUserProfile)
+  
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token = Cookies.get('token')
+
+      if (token) {
+        fetchUserProfile()
+      }
+    }
+
+    fetchUser();
+  }, [fetchUserProfile])
+
+  return (
+    <UserContext.Provider value={{fetchUserProfile}}>
+      {children}
+    </UserContext.Provider>
+  )
+}
