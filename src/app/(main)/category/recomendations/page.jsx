402b833f14ff575fbfdf10942613b9f917b3b943
juanlunaa <jp3165239@gmail.com>
2024-09-29 20:58:54 -0500

    refactor the styling, button control and info plan

--- src/app/(main)/category/recomendations/page.jsx
+++ src/app/(main)/category/recomendations/page.jsx
@@ -1,45 +1,23 @@
-import { CardPlan } from "@/components"
-import { plan } from "@/mocks/plan"
-import { textFont, titleFont } from "@/config/fonts"
 import { CardRecommendation } from "@/components/ui/recommendation-card/Card-Recommendation"
 
 export default function Recomendation() {
   return (
     <div className="flex justify-center">
       <div className="carousel-recomendation container mt-24 font-bold">
-        <h1 className={`${titleFont.className} text-2xl`}>Recomendaciones Personalizadas </h1>
+        <h1 className="text-2xl">Recomendaciones Personalizadas </h1>
 
         <hr className="mt-8 w-full"></hr>
 
-        <div className="restaurant mt-8 ">
-          <div className="title-sllider-recomendation rounded-full w-40 h-10 flex flex-wrap justify-center 
-                        content-around bg-[#37B1E2] text-white">Restaurantes</div>
-          <CardRecommendation />
-        </div>
+        <CardRecommendation title="Restaurantes"/>
 
-        <div className="tourist-site mt-8 ">
-          <div className="title-sllider-recomendation rounded-full w-40 h-10 flex flex-wrap justify-center 
-                        content-around bg-[#37B1E2] text-white">Sitios Turisticos</div>
-          <CardRecommendation />
-        </div>
+        <CardRecommendation title="Sitios Turisticos"/>
 
-        <div className="beach mt-8 ">
-          <div className="title-sllider-recomendation rounded-full w-40 h-10 flex flex-wrap justify-center 
-                        content-around bg-[#37B1E2] text-white">Playas</div>
-          <CardRecommendation />
-        </div>
+        <CardRecommendation title="Playas"/>
 
-        <div className="hotel mt-8 ">
-          <div className="title-sllider-recomendation rounded-full w-40 h-10 flex flex-wrap justify-center 
-                        content-around bg-[#37B1E2] text-white">Hotel</div>
-          <CardRecommendation />
-        </div>
+        <CardRecommendation title="Hotel"/>
 
-        <div className="extreme mt-8 ">
-          <div className="title-sllider-recomendation rounded-full w-40 h-10 flex flex-wrap justify-center 
-                        content-around bg-[#37B1E2] text-white">Extremos</div>
-          <CardRecommendation />
-        </div>
+        <CardRecommendation title="Extremos"/>
+
       </div>
     </div>
   )
--- src/components/ui/recommendation-card/Card-Recommendation.jsx
+++ src/components/ui/recommendation-card/Card-Recommendation.jsx
@@ -9,6 +9,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import { BookmarkIcon, StarIcon } from "lucide-react"
+import Image from "next/image";
 const hotels = [
   {
     name: "Hotel las Américas",
@@ -54,37 +55,52 @@ const hotels = [
   }
 ]
 
-export function CardRecommendation() {
+export function CardRecommendation({ title }) {
   return (
     <Carousel
       opts={{
         align: "start",
       }}
-      className="w-full max-w-full  mt-8"
-    >
+      className="w-full max-w-full mt-8"
+    > 
+      <div className="flex justify-between items-center mb-2">
+        <p className="rounded-full w-40 py-2 text-center bg-customBlue text-white">{title}</p>
+        <div className="flex gap-2">
+          <CarouselPrevious className="w-14 h-14 shadow-[0_0_10px_rgba(255,255,0,0.5)] hover:bg-customYellow active:hover:bg-customYellow" />
+          <CarouselNext className="w-14 h-14 shadow-[0_0_10px_rgba(255,255,0,0.5)] hover:bg-customYellow active:hover:bg-customYellow" />
+        </div>
+      </div>
       <CarouselContent>
         {hotels.map((hotel, index) => (
           <CarouselItem key={index} className="w-1 md:basis-1/3 lg:basis-1/4">
             <Card className=" w-full max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl overflow-hidden">
               <CardContent className="p-0">
-                <div className=" miniatura relative">
-                  <img
+                <div className="miniatura relative w-full h-40">
+                  <Image
                     src={hotel.image}
-                    className="w-full h-40 object-cover"
+                    alt="Image plan"
+                    fill
+                    className="object-cover"
                   />
                   <BookmarkIcon className="absolute top-2 right-2 text-white" />
                 </div>
                 <div className="p-4">
                   <h3 className="font-semibold text-lg">{hotel.name}</h3>
-                  <div className="flex items-center mt-1">
-                    {Array.from({ length: hotel.rating }).map((_, i) => (
-                      <StarIcon key={i} className="w-4 h-4 fill-current text-yellow-400" />
-                    ))}
-                    <span className="text-sm text-gray-600 ml-2">{hotel.reviews} reviews</span>
-                  </div>
-                  <div className="mt-2">
-                    <span className="text-2xl font-bold text-green-600">${hotel.price.toFixed(2)}</span>
-                    <span className="text-sm text-gray-600"> por person</span>
+                  
+                  <div className="grid grid-cols-2 mt-4">
+                    <div className="flex flex-col items-start justify-end">
+                      <div>
+                        {Array.from({ length: hotel.rating }).map((_, i) => (
+                          <StarIcon key={i} className="inline-block w-4 h-4 fill-current text-yellow-400" />
+                        ))}
+                      </div>
+                      <span className="text-xs text-gray-600">{hotel.reviews} reviews</span>
+                    </div>
+
+                    <div className="flex flex-col items-end justify-end">
+                      <span className="text-2xl font-volkhov font-bold text-green-600">${hotel.price.toFixed(2)}</span>
+                      <span className="text-xs text-gray-600"> por persona</span>
+                    </div>
                   </div>
                 </div>
               </CardContent>
@@ -92,10 +108,7 @@ export function CardRecommendation() {
           </CarouselItem>
         ))}
       </CarouselContent>
-      <div className=" relative btns-control w-[14%] h-[50px] -translate-y-[23rem] left-[80%]">
-        <CarouselPrevious className="absolute left-0  w-14 h-14 shadow-[0_0_10px_rgba(255,255,0,0.5)] hover:bg-[#FFDA32] active:hover:bg-[#FFDA32]" />
-        <CarouselNext className="absolute right-0  w-14 h-14 shadow-[0_0_10px_rgba(255,255,0,0.5)] hover:bg-[#FFDA32] active:hover:bg-[#FFDA32]" />
-      </div>
+      
     </Carousel>
   )
 }
