5f768a4d2789cc8813e134d2a5e9991303d63c89
juanlunaa <jp3165239@gmail.com>
2024-10-07 23:43:30 -0500

    fomatting code with prettier and eslint

--- src/components/ui/footer/Footer.jsx
+++ src/components/ui/footer/Footer.jsx
@@ -4,45 +4,74 @@ import Link from "next/link"
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa"
 
 export const Footer = () => {
-  const stylelinks = "text-wrap md:text-xl sm:text-base text-base";
+  const stylelinks = "text-wrap md:text-xl sm:text-base text-base"
   const stylelinstext = "text-wrap md:text-base sm:text-sm text-base"
   return (
     <div className="py-10 flex flex-col gap-4 text-center items-center sm:flex-row sm:justify-center bg-gradient-to-b from-customBlue to-white">
-
       <div className="w-[50%] sm:w-[200px] overflow-hidden">
-        <Image src="/logo.png" width={200} height={200} alt="Logo Costa Tour" className="w-full h-auto mb-4 ml-[2%] sm:mb-0" />
+        <Image
+          src="/logo.png"
+          width={200}
+          height={200}
+          alt="Logo Costa Tour"
+          className="w-full h-auto mb-4 ml-[2%] sm:mb-0"
+        />
       </div>
 
       <div className="w-[25%] flex flex-col flex-wrap content-center gap-2">
-        <h1 className={`${titleFont.className} ${stylelinks} font-bold`}>Compañia</h1>
-        <Link href="#" className={`${textFont.className} ${stylelinstext} w-max`}>
+        <h1 className={`${titleFont.className} ${stylelinks} font-bold`}>
+          Compañia
+        </h1>
+        <Link
+          href="#"
+          className={`${textFont.className} ${stylelinstext} w-max`}
+        >
           Sobre nosotros
         </Link>
       </div>
 
       <div className="w-[25%] flex flex-col flex-wrap content-center">
-        <h1 className={`${titleFont.className} ${stylelinks} font-bold`}>Contáctanos</h1>
-        <Link href="#" className={`${textFont.className} ${stylelinstext} w-max`}>
+        <h1 className={`${titleFont.className} ${stylelinks} font-bold`}>
+          Contáctanos
+        </h1>
+        <Link
+          href="#"
+          className={`${textFont.className} ${stylelinstext} w-max`}
+        >
           Centro de Ayuda
         </Link>
 
-        <Link href="#" className={`${textFont.className} ${stylelinstext} w-max`}>
+        <Link
+          href="#"
+          className={`${textFont.className} ${stylelinstext} w-max`}
+        >
           Email corporativo
         </Link>
       </div>
 
-      <div className="flex flex-wrap content-center text-center mb-[13px] flex-col gap-2 w-[25%]" >
-        <h1 className={`${titleFont.className} ${stylelinks} font-bold`}>Síguenos</h1>
+      <div className="flex flex-wrap content-center text-center mb-[13px] flex-col gap-2 w-[25%]">
+        <h1 className={`${titleFont.className} ${stylelinks} font-bold`}>
+          Síguenos
+        </h1>
         <div className="flex justify-around gap-6">
-          <Link href="#" className="text-blue-800 sm:text-xl md:text-2xl text-xl">
+          <Link
+            href="#"
+            className="text-blue-800 sm:text-xl md:text-2xl text-xl"
+          >
             <FaFacebook />
           </Link>
 
-          <Link href="#" className="text-blue-400 sm:text-xl md:text-2xl text-xl">
+          <Link
+            href="#"
+            className="text-blue-400 sm:text-xl md:text-2xl text-xl"
+          >
             <FaTwitter />
           </Link>
 
-          <Link href="#" className="text-[#C23772] sm:text-xl md:text-2xl text-xl">
+          <Link
+            href="#"
+            className="text-[#C23772] sm:text-xl md:text-2xl text-xl"
+          >
             <FaInstagram />
           </Link>
         </div>
--- src/components/ui/mapview/Map.jsx
+++ src/components/ui/mapview/Map.jsx
@@ -1,50 +1,64 @@
 "use client"
-import { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import { useEffect } from 'react';
+import { useState } from "react"
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import L from "leaflet"
+import { useEffect } from "react"
 
 const LocationMarker = () => {
-  const [position, setPosition] = useState(null);
+  const [position, setPosition] = useState(null)
 
   const map = useMapEvents({
     click() {
-      map.locate();
+      map.locate()
     },
     locationfound(e) {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
     },
-  });
+  })
 
   return position === null ? null : (
     <Marker position={position}>
       <Popup>You are here</Popup>
     </Marker>
-  );
-};
+  )
+}
 
 const MapComponent = () => {
   useEffect(() => {
     // Configurar los íconos de Leaflet
-    delete L.Icon.Default.prototype._getIconUrl;
+    delete L.Icon.Default.prototype._getIconUrl
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-    });
-  }, []);
+      iconRetinaUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+      iconUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+      shadowUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+    })
+  }, [])
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: '70%', borderRadius:'20px' }}>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: "100%", width: "70%", borderRadius: "20px" }}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <LocationMarker />
     </MapContainer>
-  );
-};
+  )
+}
 
-export default MapComponent;
+export default MapComponent
