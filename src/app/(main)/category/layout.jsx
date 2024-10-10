e49e0f8e28504b50ec3c471fbc24d58d87a357e6
Richard Fermin <drachirfer2013@hotmail.com>
2024-10-10 06:47:00 -0500

    responsive to the page category

--- src/app/(main)/category/layout.jsx
+++ src/app/(main)/category/layout.jsx
@@ -8,8 +8,8 @@ export default function CostaTourLayout({ children }) {
   const pathname = usePathname()
 
   return (
-    <div className="relative">
-      <div className="relative w-full h-[20vh] sm:h-[63vh]">
+    <div className="relative dark:bg-gray-900">
+      <div className="relative w-full h-[30vh] sm:h-[63vh]">
         <Image
           src="/categoria-banner.webp"
           alt="Banner Categorias"
@@ -18,9 +18,9 @@ export default function CostaTourLayout({ children }) {
           sizes="100vw"
         />
       </div>
-      <div className="container-main relative flex  flex-col rounded-t-[45px] top-[-50px] bg-white">
+      <div className="container-main relative flex  flex-col rounded-t-[45px] top-[-50px] bg-white dark:bg-gray-900 dark:text-white">
         <CategoryBar pathname={{ pathname }} />
-        <div className="container-info-plan mt-[4rem]">{children}</div>
+        <div className="container-info-plan sm:mt-[7rem] mt-20 dark:bg-gray-900">{children}</div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,72 +10,93 @@ module.exports = withMT({
     "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        volkhov: ["var(--font-volkhov)"],
-        mulish: ["var(--font-mulish)"],
-      },
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        customOrange: "#FFA432",
-        customBlue: "#37B1E2",
-        blueProfile: "#A7E9F2",
-        yellowProfile: "#F4B46A",
-        customYellow: "#FFDA32",
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        chart: {
-          1: "hsl(var(--chart-1))",
-          2: "hsl(var(--chart-2))",
-          3: "hsl(var(--chart-3))",
-          4: "hsl(var(--chart-4))",
-          5: "hsl(var(--chart-5))",
-        },
-      },
-      boxShadow: {
-        customBlueShadow: "0 4px 6px rgba(123, 188, 176, 0.5)",
-        customBoxShadow:
-          "0 0px 15px 2px rgb(0 0 0 / 0.1), 0 4px 6px 2px rgb(0 0 0 / 0.1)",
-      },
-      gridTemplateColumns: {
-        "auto-fit": "repeat(auto-fit, minmax(320px, 1fr))",
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			volkhov: ["var(--font-volkhov)"],
+  			mulish: ["var(--font-mulish)"]
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			customOrange: '#FFA432',
+  			customBlue: '#37B1E2',
+  			blueProfile: '#A7E9F2',
+  			yellowProfile: '#F4B46A',
+  			customYellow: '#FFDA32',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		boxShadow: {
+  			customBlueShadow: '0 4px 6px rgba(123, 188, 176, 0.5)',
+  			customBoxShadow: '0 0px 15px 2px rgb(0 0 0 / 0.1), 0 4px 6px 2px rgb(0 0 0 / 0.1)'
+  		},
+  		gridTemplateColumns: {
+  			'auto-fit': 'repeat(auto-fit, minmax(320px, 1fr))'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 })
