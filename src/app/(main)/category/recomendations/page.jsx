7dd7cad12b812798fab492faba50bf660cea4b7b
juanlunaa <jp3165239@gmail.com>
2024-10-20 17:35:44 -0500

    implement fetching plan recomendations by turist and show them in carousel by category

--- src/app/(main)/category/recomendations/page.jsx
+++ src/app/(main)/category/recomendations/page.jsx
@@ -1,51 +1,24 @@
-import { CarouselRecomendation } from "@/components"
+"use client"
 
-const planes = [
-  {
-    nombre: "Hotel las Américas",
-    miniatura: "/img-carousel/cartagena1.png?height=200&width=300",
-    rating: 4,
-    reviews: 584,
-    price: 35.0,
-  },
-  {
-    nombre: "Grand Resort",
-    miniatura: "/img-carousel/cartagena2.jpg?height=200&width=300",
-    rating: 5,
-    reviews: 1024,
-    price: 55.0,
-  },
-  {
-    nombre: "Seaside Inn",
-    miniatura: "/img-carousel/cartagena3.jpg?height=200&width=300",
-    rating: 3,
-    reviews: 302,
-    price: 28.5,
-  },
-  {
-    nombre: "Hotel las Américas",
-    miniatura: "/img-carousel/cartagena-sunset.jpg?height=200&width=300",
-    rating: 4,
-    reviews: 584,
-    price: 35.0,
-  },
-  {
-    nombre: "Hotel las Américas",
-    miniatura: "/img-carousel/cartagena-sunset.jpg?height=200&width=300",
-    rating: 4,
-    reviews: 584,
-    price: 35.0,
-  },
-  {
-    nombre: "Hotel las Américas",
-    miniatura: "/img-carousel/cartagena-sunset.jpg?height=200&width=300",
-    rating: 4,
-    reviews: 584,
-    price: 35.0,
-  },
-]
+import { CarouselRecomendation } from "@/components"
+import useUserStore from "@/hooks/useUserStore"
+import { fetchRecomendations } from "@/services/plan"
+import { useEffect, useState } from "react"
 
 export default function Recomendation() {
+  const [recomendations, setRecomendations] = useState([])
+  const { user } = useUserStore()
+  useEffect(() => {
+    const getRecomendations = async () => {
+      if (user?.dni) {
+        const data = await fetchRecomendations(user.dni)
+        setRecomendations(Array.isArray(data) ? data : [])
+      }
+    }
+
+    getRecomendations()
+  }, [user.dni])
+
   return (
     <div className="flex justify-center">
       <div className="carousel-recomendation container sm:mt-8 md:mt-14 font-bold">
@@ -55,11 +28,24 @@ export default function Recomendation() {
 
         <hr className="mt-8 w-full"></hr>
 
-        <CarouselRecomendation title="Restaurantes" planes={planes} />
-        <CarouselRecomendation title="Playas" planes={planes} />
-        <CarouselRecomendation title="Sitios Turisticos" planes={planes} />
-        <CarouselRecomendation title="Alojamientos" planes={planes} />
-        <CarouselRecomendation title="Extremos" planes={planes} />
+        <CarouselRecomendation
+          title="Restaurantes"
+          planes={recomendations?.filter((p) => p.categoria === "RESTAURANTE")}
+        />
+        <CarouselRecomendation
+          title="Sitios Turisticos"
+          planes={recomendations?.filter(
+            (p) => p.categoria === "SITIO_TURISTICO"
+          )}
+        />
+        <CarouselRecomendation
+          title="Playas"
+          planes={recomendations?.filter((p) => p.categoria === "PLAYA")}
+        />
+        <CarouselRecomendation
+          title="Alojamientos"
+          planes={recomendations?.filter((p) => p.categoria === "ALOJAMIENTO")}
+        />
       </div>
     </div>
   )
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,11 @@
+import { BACKEND_SERVER } from "@/env"
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
+}
+
+export function formatSrcImage(src) {
+  return src.startsWith("http") ? src : `${BACKEND_SERVER}${src}`
 }
