cb93f2c9ca96c7f22bde89e1ce08b03de051b377
juanlunaa <jp3165239@gmail.com>
2024-10-19 20:16:39 -0500

    refactor ScheduleSelector component view and implement react-hook-form

--- src/components/ScheduleSelector.jsx
+++ src/components/ScheduleSelector.jsx
@@ -1,139 +1,82 @@
 "use client"
 
-import React, { useState } from "react"
+import { cn } from "@/lib/utils"
 import { Input } from "./ui/input"
 import { Trash2, AlarmClockPlus } from "lucide-react"
+import { Label } from "./ui/label"
 
-const daysOfWeek = [
-  "Lunes",
-  "Martes",
-  "Miércoles",
-  "Jueves",
-  "Viernes",
-  "Sábado",
-  "Domingo",
+const hoursDefault = [
+  "05:00",
+  "06:00",
+  "07:00",
+  "08:00",
+  "09:00",
+  "10:00",
+  "11:00",
+  "12:00",
+  "13:00",
+  "14:00",
+  "15:00",
+  "16:00",
+  "17:00",
+  "18:00",
+  "19:00",
+  "20:00",
+  "21:00",
+  "22:00",
 ]
 
-const HourSelectorByDay = ({
-  day,
-  schedule,
-  handleHourChange,
-  removeHour,
-  addHour,
-}) => {
-  const dayHours = Object.keys(schedule).find((key) => key === day)
-
-  return dayHours ? (
-    <div className="flex flex-col items-center gap-1 mt-2 w-[184px] rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background">
-      {schedule[day].map((hour, hourIndex) => (
-        <div key={hourIndex} className="flex gap-1">
-          <Input
-            type="time"
-            className="w-fit h-8"
-            value={hour}
-            onChange={(e) => handleHourChange(day, hourIndex, e)}
+const HoursColumn = ({ hours, day, value, toggleHour }) => {
+  return (
+    <div className="flex flex-col gap-2">
+      {hours.map((hour, hourIndex) => (
+        <div key={hourIndex}>
+          <input
+            type="checkbox"
+            id={`check-${day}-${hour}`}
+            className="hidden peer"
+            onChange={() => {
+              toggleHour(day, hour)
+            }}
+            checked={value[day]?.includes(hour)}
           />
-          <div className="flex items-center justify-center grow-0 h-8 w-8 bg-red-700/40 border border-red-500 rounded-md ">
-            <Trash2
-              className="cursor-pointer text-red-700 h-5 w-5"
-              onClick={() => removeHour(day, hourIndex)}
-            />
-          </div>
+          <label
+            htmlFor={`check-${day}-${hour}`}
+            className="block w-full h-6 cursor-pointer p-1 rounded-md transition-colors duration-300 bg-gray-400 
+        peer-checked:bg-customBlue/50"
+          />
         </div>
       ))}
-
-      <span
-        className="cursor-pointer rounded-md p-1 border border-input bg-background hover:bg-accent hover:text-accent-foreground"
-        onClick={() => addHour(day)}
-      >
-        <AlarmClockPlus />
-      </span>
     </div>
-  ) : null
+  )
 }
 
-export const ScheduleSelector = () => {
-  const [schedule, setSchedule] = useState({}) // Objeto para almacenar días y sus horas
-
-  // Función para agregar una hora a un día específico
-  const addHour = (day) => {
-    setSchedule((prevSchedule) => ({
-      ...prevSchedule,
-      [day]: [...(prevSchedule[day] || []), ""], // Añadir un campo vacío para la nueva hora
-    }))
-  }
-
-  // Función para manejar cambios en una hora específica de un día
-  const handleHourChange = (day, hourIndex, event) => {
-    const newHours = [...schedule[day]]
-    newHours[hourIndex] = event.target.value // Actualizar la hora
-    setSchedule((prevSchedule) => ({
-      ...prevSchedule,
-      [day]: newHours,
-    }))
-  }
-
-  // Función para eliminar una hora específica de un día
-  const removeHour = (day, hourIndex) => {
-    const newHours = schedule[day].filter((_, index) => index !== hourIndex)
-
-    const newSchedule = { ...schedule }
-    if (newSchedule[day].length === 1) {
-      delete newSchedule[day] // Si solo queda una hora a eliminar tambien se elimina el día
-      setSchedule(newSchedule)
-    } else {
-      setSchedule((prevSchedule) => ({
-        ...prevSchedule,
-        [day]: newHours,
-      }))
-    }
-  }
-
-  // Función para agregar un día a la programación
-  const toggleDay = (day) => {
-    setSchedule((prevSchedule) => {
-      if (prevSchedule[day]) {
-        const newSchedule = { ...prevSchedule }
-        delete newSchedule[day] // Si el día ya estaba seleccionado, lo eliminamos
-        return newSchedule
-      } else {
-        return { ...prevSchedule, [day]: [""] } // Si no, lo añadimos con un array vacío de horas
-      }
-    })
-  }
-
+export const ScheduleSelector = ({ days, value, toggleHour }) => {
   return (
-    <div>
-      <h2>Seleccionar días de la semana</h2>
-      {/* Checkboxes para seleccionar los días de la semana */}
-      <div className="flex flex-col gap-2">
-        {daysOfWeek.map((day) => (
-          <div key={day}>
-            <input
-              type="checkbox"
-              id={`check-${day}`}
-              className="hidden peer"
-              onChange={() => toggleDay(day)}
-              checked={!!schedule[day]}
-            />
-            <label
-              htmlFor={`check-${day}`}
-              className="cursor-pointer p-1 rounded-md transition-colors duration-300 bg-gray-200 
-                peer-checked:bg-customBlue/50"
-            >
-              {day}
-            </label>
-            <HourSelectorByDay
-              day={day}
-              schedule={schedule}
-              handleHourChange={handleHourChange}
-              removeHour={removeHour}
-              addHour={addHour}
-            />
-          </div>
-        ))}
+    <div className="grid grid-cols-8 gap-1 mt-2 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background">
+      <div className="text-center">
+        <p className="font-bold dark:text-white mb-2">Horas</p>
+        <div className="flex flex-col gap-2">
+          {hoursDefault.map((hour, index) => (
+            <p key={index} className="w-full h-6 font-bold dark:text-white">
+              {hour}
+            </p>
+          ))}
+        </div>
       </div>
-      <pre>{JSON.stringify(schedule, null, 2)}</pre> {/* Para depuración */}
+
+      {days.map((day, index) => (
+        <div key={day} className="text-center">
+          <p className="font-bold dark:text-white mb-2">{day}</p>
+          <HoursColumn
+            hours={hoursDefault}
+            day={day}
+            value={value}
+            toggleHour={toggleHour}
+            indexDay={index}
+          />
+        </div>
+      ))}
     </div>
   )
 }
