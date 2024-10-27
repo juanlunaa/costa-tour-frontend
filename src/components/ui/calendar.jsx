aad18da86377db38ed647bd9cc13f77799906f39
juanlunaa <jp3165239@gmail.com>
2024-10-27 14:38:20 -0500

    fomatting code (eslint & prettier)

--- src/components/ui/calendar.jsx
+++ src/components/ui/calendar.jsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
@@ -6,14 +6,9 @@ import { DayPicker } from "react-day-picker"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
-function Calendar({
-  className,
-  classNames,
-  showOutsideDays = true,
-  ...props
-}) {
+function Calendar({ className, classNames, showOutsideDays = true, ...props }) {
   return (
-    (<DayPicker
+    <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
@@ -54,8 +49,9 @@ function Calendar({
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
-      {...props} />)
-  );
+      {...props}
+    />
+  )
 }
 Calendar.displayName = "Calendar"
 
--- src/components/ui/toggleCalendar/dateCalendar.jsx
+++ src/components/ui/toggleCalendar/dateCalendar.jsx
@@ -35,23 +35,29 @@
 
 "use client"
 
-import React, { useState } from "react";
-import { format } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
+import React, { useState } from "react"
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
 
-import { cn } from "@/lib/utils"; 
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"; 
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { es } from "date-fns/locale"
 
 export const DatePickerDemo = ({ availableDays = [] }) => {
-  const [date, setDate] = useState(null);
+  const [date, setDate] = useState(null)
 
   //deshabilita los días no disponibles
   const isDateDisabled = (date) => {
-    const dayName = format(date, 'EEEE'); 
-    return !availableDays.includes(dayName);
-  };
+    console.log(date)
+    const dayName = format(date, "EEEE")
+    return !availableDays.includes(dayName)
+  }
 
   return (
     <Popover>
@@ -64,19 +70,24 @@ export const DatePickerDemo = ({ availableDays = [] }) => {
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? <span className="truncate">{ format(date, "PPP")}</span> : <span className="text-ellipsis overflow-hidden">Pick a date</span>}
+          {date ? (
+            <span className="truncate">{format(date, "PPP")}</span>
+          ) : (
+            <span className="text-ellipsis overflow-hidden">Pick a date</span>
+          )}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-full p-0 max-w-sm sm:max-w-xs">
         <Calendar
+          locale={es}
           mode="single"
           selected={date}
           onSelect={setDate}
           initialFocus
-          disabled={isDateDisabled} 
+          disabled={isDateDisabled}
           className="w-full"
         />
       </PopoverContent>
     </Popover>
-  );
-};
+  )
+}
