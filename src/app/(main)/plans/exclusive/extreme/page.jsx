3d9cc8f8b21e4ca7c2ec2422507b7c27ff6175b9
juanlunaa <jp3165239@gmail.com>
2024-11-17 21:37:35 -0500

    add FiltersExtreme component for advanced filtering options and refactor Extreme and NightLife page to utilize fetch plans to client side

--- src/app/(main)/plans/exclusive/extreme/page.jsx
+++ src/app/(main)/plans/exclusive/extreme/page.jsx
@@ -1,21 +1,110 @@
+"use client"
+
 import LayoutExclusive from "../layout"
 import { CardExtreme } from "@/components/plan/CardBannerExtreme"
 import { fetchAllPlans, fetchAllPlansExclusives } from "@/services/plan"
-import CheckboxGroupDemo from "@/components/ui/checkbox-group/Checkbox"
+import CheckboxGroupDemo from "@/components/ui/checkbox-group/FiltersPlace"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { CardPlan } from "@/components"
 import clsx from "clsx"
-export default async function ExtremePage() {
+import { useEffect, useState } from "react"
+import FiltersExtreme from "@/components/plan/FiltersExtreme"
+
+export default function ExtremePage() {
   const extremeBanner = (
     <img src="/banner-extremo.png" className="h-auto max-h-[75vh] w-full" />
   )
-  const plans = await fetchAllPlansExclusives()
-  console.log(plans)
-  const validPlans = Array.isArray(plans) ? plans : []
+
+  const [plans, setPlanes] = useState([])
+
+  const [filtros, setFiltros] = useState({
+    partesDelDia: [],
+    caracteristicaAventura: [],
+  })
+
+  const toggleFiltro = (filtroTipo, opcion) => {
+    setFiltros((prev) => {
+      const isSelect = prev[filtroTipo].some((item) => item.id === opcion.id)
+
+      return {
+        ...prev,
+        [filtroTipo]: isSelect
+          ? prev[filtroTipo].filter((item) => item.id !== opcion.id)
+          : [...prev[filtroTipo], opcion],
+      }
+    })
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const plans = await fetchAllPlansExclusives()
+      setPlanes(plans)
+    }
+
+    fetchData()
+  }, [])
   const textShadow = clsx(
     "[text-shadow:_0_3px_0_rgb(255_255_255_/_100%),_0_-3px_0_rgb(255_255_255_/_100%),_3px_0_0_rgb(255_255_255_/_100%),_-3px_0_0_rgb(255_255_255_/_100%)]"
   )
-  const extremos = validPlans?.filter((p) => p.categoria === "EXTREMO")
+  const extremos = plans?.filter((p) => p.categoria === "EXTREMO")
+
+  const filtrarPorParteDelDia = (planes, filtro) => {
+    if (filtro.length === 0) return planes
+
+    return planes.filter((plan) => {
+      // Verifico la horas de disponibilidad
+      return plan.disponibilidad.some((dia) => {
+        return dia.horas.some((hora) => {
+          const horaPlan = parseInt(hora.split(":")[0], 10)
+
+          // Verificar si alguna de las partes del día coincide con la hora del plan
+          return filtro.some((values) => {
+            const valorParte = parseInt(values.value.split(":")[0], 10)
+
+            if (values.title.includes("mañana")) {
+              // Empieza antes de las 12pm
+              return horaPlan < valorParte
+            } else if (values.title.includes("tarde")) {
+              // Empieza desde las 12pm
+              return horaPlan >= valorParte && horaPlan < 17
+            } else if (values.title.includes("noche")) {
+              // Empieza después de las 5pm
+              return horaPlan >= valorParte
+            }
+
+            return false
+          })
+        })
+      })
+    })
+  }
+
+  const filtrarPorAventura = (planes, filtro) => {
+    if (filtro.length === 0) return planes
+
+    return planes.filter((plan) =>
+      filtro.some((values) =>
+        plan.caracteristicas.some((c) => c.caracteristica === values.value)
+      )
+    )
+  }
+
+  const filtrarPlanes = (planes) => {
+    let resultado = [...planes]
+
+    if (filtros.partesDelDia.length > 0) {
+      resultado = filtrarPorParteDelDia(resultado, filtros.partesDelDia)
+    }
+
+    if (filtros.caracteristicaAventura.length > 0) {
+      resultado = filtrarPorAventura(resultado, filtros.caracteristicaAventura)
+    }
+
+    return resultado
+  }
+
+  const planesFiltrados = filtrarPlanes(extremos)
+
   return (
     <div className="pt-20 mb-12 relative">
       <div className="relative">
@@ -67,22 +156,24 @@ export default async function ExtremePage() {
       </div>
 
       <div className="grid-filter-tabContent flex flex-col items-center sm:items-start sm:flex-row sm:justify-between sm:mt-24 mt-8 sm:gap-1 gap-5">
-        <div
+        <FiltersExtreme filtros={filtros} toggleFiltro={toggleFiltro} />
+        {/* <div
           className="filter shadow-customBoxShadow rounded-sm sm:max-w-[150px] md:max-w-[200px] max-w-[600px] 
                           w-[90%] sm:ml-[3%] h-fit sm:flex-row dark:bg-gray-800"
         >
           <CheckboxGroupDemo />
-        </div>
+        </div> */}
         {/* <ScrollArea className="relative h-[700px] sm:mr-1  md:mr-5 overflow-auto"> */}
         <div className="flex justify-end">
           <div className=" grid grid-cols-2 justify-items-center sm:grid-cols-2 md:gap-4 sm:gap-y-5 gap-y-4 dark:text-white">
-            {extremos.map((p) => (
+            {planesFiltrados.map((p) => (
               <CardPlan
                 key={p.id}
                 id={p.id}
                 nombre={p.nombre}
                 miniatura={p.miniatura}
                 descripcion={p.descripcion}
+                calificacionPromedio={p.calificacionPromedio}
                 href={`/plans/exclusive/extreme/info/${p.id}`}
               />
             ))}
