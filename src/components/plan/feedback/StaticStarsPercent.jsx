d289774de3213a23a6640c7714b9a32906fd74c9
juanlunaa <jp3165239@gmail.com>
2024-11-17 21:35:33 -0500

    add average rating display to saved plans and integrate StaticStarsPercent component

--- src/components/plan/feedback/StaticStarsPercent.jsx
+++ src/components/plan/feedback/StaticStarsPercent.jsx
@@ -1,32 +1,42 @@
 import { cn } from "@/lib/utils"
 import { IoMdStar } from "react-icons/io"
 
-const StaticStarsPercent = ({ value }) => {
-  const percentage = (new Number(value).toFixed(2) / 5) * 100
+const StaticStarsPercent = ({ value, className }) => {
+  const percentage = ((new Number(value).toFixed(2) / 5) * 100).toFixed(0)
+
+  const divOrangeStars = cn(
+    `absolute top-0 left-0 overflow-hidden whitespace-nowrap w-[${20}%]`
+  )
+
+  console.log(divOrangeStars)
 
   return (
-    <div className="mt-2 w-fit relative">
+    <div className="w-fit relative">
       {/* Estrellas grises */}
       <div>
         {Array.from({ length: 5 }).map((_, i) => (
           <IoMdStar
             key={i}
-            className="inline-block md:w-12 md:h-12 sm:w-8 sm:h-8 w-6 h-6 text-gray-300 fill-current"
+            className={cn(
+              "inline-block md:w-12 md:h-12 sm:w-8 sm:h-8 w-6 h-6 text-gray-300 fill-current",
+              className
+            )}
           />
         ))}
       </div>
 
       {/* Estrellas puntuadas */}
       <div
-        className={cn(
-          "absolute top-0 left-0 overflow-hidden whitespace-nowrap",
-          `w-[${percentage}%]`
-        )}
+        className="absolute top-0 left-0 overflow-hidden whitespace-nowrap"
+        style={{ width: `${percentage}%` }}
       >
         {Array.from({ length: 5 }).map((_, i) => (
           <IoMdStar
             key={i}
-            className="inline-block md:w-12 md:h-12 sm:w-8 sm:h-8 w-6 h-6 text-customOrange fill-current"
+            className={cn(
+              "inline-block md:w-12 md:h-12 sm:w-8 sm:h-8 w-6 h-6 text-customOrange fill-current",
+              className
+            )}
           />
         ))}
       </div>
